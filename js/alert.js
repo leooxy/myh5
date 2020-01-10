004559dfe81e31c547f525e575dd46a184d0b066
leooxy <59411927+leooxy@users.noreply.github.com>
2020-01-11 02:55:10 +0800

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,13 @@
-
+function taolu()
+      {
+      alert("你猜一会儿会发生什么");
+      alert("再等等");
+      alert("也许有奇迹");
+      }
+      alert("1========你怎么打开了这个网页");
+        
+        taolu();
+        taolu();
+        taolu();
+      
+      alert("2========这是最后一句了，88");
